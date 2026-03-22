b3ff89cebd331f24889143966ec1dfa6f7d4998e
NgJoonHin <n.joonhin@digipen.edu>
2026-03-23 05:35:00 +0800

    temp fix uiconfig secondary font

--- Project/UIConfig.h
+++ Project/UIConfig.h
@@ -4,6 +4,6 @@
 // --- GLOBAL FONT CONFIGURATION ---
 // Change these paths to update the font for the entire game.
 constexpr const char* PRIMARY_FONT_PATH = "Assets/Jersey25-Regular.ttf";
-constexpr const char* SECONDARY_FONT_PATH = "Assets/Exo2/Jersey25-Regular.ttf";
+constexpr const char* SECONDARY_FONT_PATH = "Assets/Jersey25-Regular.ttf";
 
 #endif // !_UI_CONFIG_H_
